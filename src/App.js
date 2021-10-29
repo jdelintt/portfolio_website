f72b71dc997bda6ded2c7561bad6e618efd577d2
Jesus DeLintt <jesus.delintt@gmail.com>
2021-10-28 21:46:21 -0700

    baby change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with Jesus DeLintt
         </a>
       </header>
     </div>
