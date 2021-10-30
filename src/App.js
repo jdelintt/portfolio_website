0d9df8af96fe6391b2a52775ab740be236d8207e
Jesus DeLintt <jesus.delintt@gmail.com>
2021-10-30 15:05:01 -0700

    Formatting for landing page

--- src/App.js
+++ src/App.js
@@ -1,31 +1,21 @@
 
 import './App.css';
 import 'antd/dist/antd.css';
-import { Menu } from 'antd';
-import { Layout } from 'antd';
+import { Menu, Layout, Row, Col } from 'antd';
 import { MailOutlined } from '@ant-design/icons';
+import Icon from './Figma'
+
 
 function App() {
-  const { Header, Footer, Sider, Content } = Layout;
+  const { Header, Footer, Content } = Layout;
   return (
     <>
-      <Layout style={{backgroundColor: "#b5f5ec"}}>
-        <Sider style={{backgroundColor: "#b5f5ec"}}>
-          <Menu style={{backgroundColor: "#b5f5ec"}}>
-              <Menu.Item icon={<MailOutlined />}>
-
-              </Menu.Item>
-              <Menu.Item>
-                
-              </Menu.Item>
-              <Menu.Item>
-                
-              </Menu.Item>
-          </Menu>
-        </Sider>
         <Layout style={{backgroundColor: "#b5f5ec"}}>
           <Header style={{backgroundColor: "#b5f5ec"}}>
             <Menu mode="horizontal" style={{backgroundColor: "#b5f5ec"}}>
+                <Menu.Item icon={<Icon/>} style={{marginTop: '3px', marginRight: '20px'}}>
+                </Menu.Item>
+
                 <Menu.Item>
                   Home
                 </Menu.Item>
@@ -43,32 +33,45 @@ function App() {
                 </Menu.Item>
               </Menu>
           </Header>
-          <Content style={{marginBottom: '150px'}}>
-            <div>
-                <h1>
-                  Welcome everyone to my personal portfolio site!
-                </h1>
-                <h2>
-                  I am looking to grow my skills in software engineering and data science. Check out my experience and recent projects in
-                  both fields
-                </h2>
-            </div>
-          </Content >
-          <Footer style={{backgroundColor: "#b5f5ec"}}>
-            <Menu style={{backgroundColor: "#b5f5ec"}}>
-                <Menu.Item icon={<MailOutlined />}>
+          <Content style={{marginBottom: '118px', marginTop: '40px'}}>
+            <Row>
+              <Col span={6}>
+              
+              </Col>
+              <Col span={10}>
+              <div>
+                  <h1>
+                    Welcome everyone to my personal portfolio site!
+                  </h1>
+                  <h2>
+                    I am looking to grow my skills in software engineering and data science. Check out my experience and recent projects in
+                    both fields
+                  </h2>
+                </div>
+              </Col>
+              <Col span={8}>
+              
+              </Col>
+            </Row>
 
-                </Menu.Item>
-                <Menu.Item>
-                  
-                </Menu.Item>
-                <Menu.Item>
-                  
-                </Menu.Item>
-              </Menu>
+
+          </Content >
+          <Footer style={{backgroundColor: "#b5f5ec"}}>  
+          <Row>
+            <Col span={6}>
+            </Col>
+            <Col span={6} style={{alignContent: 'center', justifyContent: 'center'}}>
+            <MailOutlined /> 
+            </Col>
+            <Col span={6}>
+            <MailOutlined /> 
+            </Col>
+            <Col span={6}>
+            <MailOutlined />   
+            </Col>
+          </Row>
           </Footer>
         </Layout>
-      </Layout>
     </>
   );
 }
---
+++ src/Figma.jsx
@@ -0,0 +1,105 @@
+import * as React from "react";
+
+function Icon(props) {
+  return (
+    <svg
+      width={59}
+      height={57}
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <g filter="url(#prefix__filter0_ddi_2:6)">
+        <path
+          d="M4 8.863c19.579-11.847 30.748-11.788 51 0l-7.244 33.305L29.5 49l-17.386-6.832L4 8.863z"
+          fill="#FADB14"
+        />
+      </g>
+      <g filter="url(#prefix__filter1_d_2:6)">
+        <path
+          d="M20.048 18.337h2.862l-1.67 9.844a5.493 5.493 0 01-.899 2.412 4.812 4.812 0 01-1.855 1.631c-.756.378-1.586.557-2.49.537-1.342-.026-2.377-.423-3.106-1.191-.73-.768-1.074-1.843-1.035-3.223l2.88.01c-.097 1.341.375 2.021 1.417 2.041a1.87 1.87 0 001.445-.566c.397-.398.654-.948.771-1.65l1.68-9.845zM34.325 30.76c-.507.645-1.253 1.143-2.236 1.495-.976.345-2.08.507-3.31.488-.97-.007-1.823-.218-2.559-.635-.736-.423-1.315-1.029-1.738-1.816-.424-.788-.664-1.687-.723-2.696-.065-.937.033-2.037.293-3.3.267-1.27.72-2.38 1.358-3.33.644-.958 1.416-1.67 2.314-2.14.898-.474 1.894-.702 2.988-.683 1.485.026 2.647.446 3.487 1.26.84.807 1.318 1.966 1.435 3.476l-2.754-.01c-.072-.794-.283-1.38-.635-1.757-.351-.378-.875-.58-1.572-.606-1.204-.039-2.155.492-2.851 1.592-.69 1.094-1.084 2.682-1.182 4.766-.065 1.113.104 1.976.508 2.588.41.612 1.032.924 1.865.937.97.026 1.797-.205 2.48-.693l.479-2.451h-2.598l.381-2.159h5.43l-.86 5.674zm1.924 1.797l2.471-14.218 4.082.01c.957.019 1.816.237 2.578.654a4.835 4.835 0 011.865 1.777c.476.775.762 1.647.86 2.617a8.637 8.637 0 01-.02 1.738l-.068.518c-.287 2.083-1.107 3.756-2.461 5.02-1.348 1.256-2.985 1.884-4.912 1.884h-4.395zm4.922-11.845l-1.65 9.492 1.191.01c1.315 0 2.37-.482 3.164-1.446.801-.963 1.26-2.418 1.377-4.365l.02-.313c.02-1.074-.186-1.897-.616-2.47-.43-.573-1.07-.873-1.923-.899l-1.563-.01z"
+          fill="#000"
+        />
+      </g>
+      <defs>
+        <filter
+          id="prefix__filter0_ddi_2:6"
+          x={0}
+          y={0}
+          width={59}
+          height={57}
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity={0} result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy={4} />
+          <feGaussianBlur stdDeviation={2} />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+          <feBlend in2="BackgroundImageFix" result="effect1_dropShadow_2:6" />
+          <feColorMatrix
+            in="SourceAlpha"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy={4} />
+          <feGaussianBlur stdDeviation={2} />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+          <feBlend
+            in2="effect1_dropShadow_2:6"
+            result="effect2_dropShadow_2:6"
+          />
+          <feBlend
+            in="SourceGraphic"
+            in2="effect2_dropShadow_2:6"
+            result="shape"
+          />
+          <feColorMatrix
+            in="SourceAlpha"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy={4} />
+          <feGaussianBlur stdDeviation={2} />
+          <feComposite in2="hardAlpha" operator="arithmetic" k2={-1} k3={1} />
+          <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+          <feBlend in2="shape" result="effect3_innerShadow_2:6" />
+        </filter>
+        <filter
+          id="prefix__filter1_d_2:6"
+          x={7.796}
+          y={18.113}
+          width={44.387}
+          height={22.678}
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity={0} result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy={4} />
+          <feGaussianBlur stdDeviation={2} />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+          <feBlend in2="BackgroundImageFix" result="effect1_dropShadow_2:6" />
+          <feBlend
+            in="SourceGraphic"
+            in2="effect1_dropShadow_2:6"
+            result="shape"
+          />
+        </filter>
+      </defs>
+    </svg>
+  );
+}
+
+export default Icon;
