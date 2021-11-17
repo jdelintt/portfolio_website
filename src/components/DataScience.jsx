1eaa864761e6bab4714ff49f3c9211277bc4ba64
Jesus DeLintt <jesus.delintt@gmail.com>
2021-11-16 19:51:37 -0800

    changes

--- src/components/DataScience.jsx
+++ src/components/DataScience.jsx
@@ -8,7 +8,7 @@ function DataScience () {
             <div style={{marginTop:'20px', marginBottom: '20px', border: 'solid', width: '60vw',
             display:'flex', flexDirection: 'column', alignItems: 'center'}} className='border_show fade_in_component'>
                 <h1>
-                    Data Visualizations
+                    Data Visualizations for Jesus
                 </h1>
                 <iframe style={{border: 0, frameborder: '0'}} width="375" height="283" src="https://datastudio.google.com/embed/reporting/1bd3abee-a342-4be7-a882-6fdc5d64f617/page/PfJSC" allowFullScreen={true}></iframe>
             </div>
